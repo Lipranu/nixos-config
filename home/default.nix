689372f75b32f386594d68d66f3ae1d9a21da2ac
Igor Belousov <lipranu@gmail.com>
2020-11-20 14:47:17 +0000

    Added homeDirectory key to home configuration

--- home/default.nix
+++ home/default.nix
@@ -1,9 +1,16 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.lipranu.programs.git = {
-    enable = true;
-    userEmail = "lipranu@gmail.com";
-    userName = "Igor Belousov";
+  
+  home-manager.users.lipranu = rec {
+
+    home.homeDirectory = "/home/lipranu";
+
+    programs.git = {
+      enable = true;
+      userEmail = "lipranu@gmail.com";
+      userName = "Igor Belousov";
+    };
+
   };
 }
