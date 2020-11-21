673f38fd83f270b3d7622360dba3fd659a3c5b18
Igor Belousov <lipranu@gmail.com>
2020-11-22 04:07:03 +0700

    Changed path to termonad config in home module

--- home/termonad/default.nix
+++ home/termonad/default.nix
@@ -1,4 +1,6 @@
 {
-  home-manager.users.lipranu.home.file.".config/termonad/termonad.hs".text
-    = builtins.readFile ./termonad.hs;
+#  home-manager.users.lipranu.home.file.".config/termonad/termonad.hs".text
+#    = builtins.readFile ./termonad.hs;
+  home-manager.users.lipranu.xdg.configFile."termonad/termonad.hs".source
+    = ./termonad.hs;
 }
