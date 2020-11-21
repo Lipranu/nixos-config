d0975e43d1761610090919350b26a0be9f0085f5
Igor Belousov <lipranu@gmail.com>
2020-11-22 04:43:22 +0700

    Garbage removed

--- home/termonad/default.nix
+++ home/termonad/default.nix
@@ -1,6 +1,4 @@
 {
-#  home-manager.users.lipranu.home.file.".config/termonad/termonad.hs".text
-#    = builtins.readFile ./termonad.hs;
   home-manager.users.lipranu.xdg.configFile."termonad/termonad.hs".source
     = ./termonad.hs;
 }
