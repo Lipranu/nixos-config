4aac4cc3fd0958e650ca06c4b149bf8ed768a51c
Igor Belousov <lipranu@gmail.com>
2020-11-23 02:40:50 +0700

    Added binary caches paths to nix configuration

--- system/nix.nix
+++ system/nix.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }: {
   nix = {
+
     package = pkgs.nixUnstable;
     extraOptions = "experimental-features = nix-command flakes";
+
+    binaryCaches = [
+      "https://cache.nixos.org/"
+      "https://nixcache.reflex-frp.org"
+    ];
+
+    binaryCachePublicKeys = [
+      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+    ];
+
   };
 }
