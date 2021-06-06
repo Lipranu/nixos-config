d38cb8861f8434876f42d4e5dd59814edb934f52
Igor Belousov <lipranu@gmail.com>
2021-06-06 21:25:26 +0700

    Added new network point

--- system/network.nix
+++ system/network.nix
@@ -5,8 +5,10 @@
 
     wireless = {
       enable = true;
-      networks.Lipranu.pskRaw =
-        "104b0802bfb3dd8db0cca89fac8775988a5625fc3b45bfbf4ebdfa0a4e589e7c";
+      networks = {
+        Lipranu.pskRaw = "104b0802bfb3dd8db0cca89fac8775988a5625fc3b45bfbf4ebdfa0a4e589e7c";
+        Focus.pskRaw = "7fed4b95db04399f3d83e2fb79161cff379f7d7f154114e614fec744617f5125";
+      };
     };
 
     interfaces = {
